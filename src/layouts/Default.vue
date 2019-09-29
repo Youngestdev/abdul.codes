9a8e0673cfb5ead31fc19a3225883e37a432f709
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2019-09-29 14:15:51 +0100

    customization

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="layout">
     <slot/>
+    <div class="footer">
+      <p>
+        Check my <a class="link" href="//github.com/Youngestdev">GitHub</a>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -10,14 +15,13 @@ body {
     "Helvetica Neue", Arial, sans-serif;
   margin: 0;
   padding: 0;
+  padding-bottom: 20px;
   line-height: 1.5;
   border: 15px solid #1184e2;
   min-height: 100vh;
-  width: fit-content;
 }
 
 .layout {
-  max-width: max-content;
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
@@ -30,7 +34,7 @@ body {
 }
 
 .link {
-  border: 2px dashed #fecd43;
+  border: 2px solid transparent;
   padding: 7px;
   color: #333;
   text-decoration: none;
