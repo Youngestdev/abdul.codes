cd3514e41fd3c69d052f099e6dd08937705cc1a2
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2020-08-11 10:59:24 +0100

    Changes upon changes

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,15 +6,21 @@
     </header>
     <section class="posts">
       <PostList v-for="edge in $page.allPost.edges" :key="edge.node.id" :post="edge.node" />
+      <br />
+      <hr />
+      <Pager :info="$page.allPost.pageInfo" class="nav"/>
     </section>
   </Layout>
 </template>
 
 <script>
 import PostList from "@/components/PostList";
+import { Pager } from 'gridsome';
+
 export default {
   components: {
-    PostList
+    PostList,
+    Pager
   },
   metaInfo: {
     title: "A simple blog"
@@ -23,13 +29,17 @@ export default {
 </script>
 
 <page-query>
-query {
+query ($page: Int) {
   metaData {
     siteName
     siteDescription
   }
-  allPost {
+  allPost(perPage: 5, page: $page) @paginate {
     totalCount
+    pageInfo {
+      totalPages
+      currentPage
+    }
     edges {
       node {
         id
@@ -47,7 +57,7 @@ query {
 
 <style>
 .header {
-  font-family: "Stylish";
+  font-family: "Stylish",serif;
   font-size: 15px;
   text-align: center;
   line-height: 20px;
@@ -56,4 +66,35 @@ query {
 .header p {
   font-weight: 100;
 }
+
+.pag-link {
+  border-radius: 4px;
+}
+
+.pag-link.active {
+  border-radius: 4px;
+}
+
+.nav {
+  display: inline-block;
+  place-items: center;
+}
+
+.nav a {
+  color: black;
+  float: left;
+  padding: 8px 16px;
+  text-decoration: none;
+}
+
+.nav a.active {
+  background-color: #1184e2;
+  border-radius: 5px;
+}
+
+.nav a:hover:not(.active) {
+  background-color: lightblue;
+  border-radius: 5px;
+}
+
 </style>
