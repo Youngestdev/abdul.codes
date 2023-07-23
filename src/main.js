d65da21bda5f2ae61dd4b2185ec8d3789a7595cc
Abdulazeez Abdulazeez Adeshina <31009679+Youngestdev@users.noreply.github.com>
2023-07-23 17:31:02 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import VueDisqus from 'vue-disqus'
+import 'katex/dist/katex.min.css'
 import DefaultLayout from '~/layouts/Default.vue'
 require("./code.css");
 
