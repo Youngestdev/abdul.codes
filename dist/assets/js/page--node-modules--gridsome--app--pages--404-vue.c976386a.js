fbc26b985924a8a55acaee7195f584a1828f88c1
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2025-02-20 19:37:41 +0100

    at this point, let the world burn.

---
+++ dist/assets/js/page--node-modules--gridsome--app--pages--404-vue.c976386a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{384:function(n,t,u){"use strict";u.r(t);var o=u(97),s=Object(o.a)({},(function(){return(0,this._self._c)("h1",[this._v("404 - not found")])}),[],!1,null,null,null);t.default=s.exports}}]);
